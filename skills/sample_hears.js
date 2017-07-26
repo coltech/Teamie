bcdb0757cc27262546af3ffd5349cb1ab611421e
THADMIN\colbil1 <coltech@gmail.com>
2017-07-26 09:56:32 -0400

    no message

---
+++ skills/sample_hears.js
@@ -0,0 +1,97 @@
+/*
+
+WHAT IS THIS?
+
+This module demonstrates simple uses of Botkit's `hears` handler functions.
+
+In these examples, Botkit is configured to listen for certain phrases, and then
+respond immediately with a single line response.
+
+*/
+
+var wordfilter = require('wordfilter');
+
+module.exports = function(controller) {
+
+    /* Collect some very simple runtime stats for use in the uptime/debug command */
+    var stats = {
+        triggers: 0,
+        convos: 0,
+    }
+
+    controller.on('heard_trigger', function() {
+        stats.triggers++;
+    });
+
+    controller.on('conversationStarted', function() {
+        stats.convos++;
+    });
+
+
+    controller.hears(['^uptime','^debug'], 'direct_message,direct_mention', function(bot, message) {
+
+        bot.createConversation(message, function(err, convo) {
+            if (!err) {
+                convo.setVar('uptime', formatUptime(process.uptime()));
+                convo.setVar('convos', stats.convos);
+                convo.setVar('triggers', stats.triggers);
+
+                convo.say('My main process has been online for {{vars.uptime}}. Since booting, I have heard {{vars.triggers}} triggers, and conducted {{vars.convos}} conversations.');
+                convo.activate();
+            }
+        });
+
+    });
+	
+	controller.hears(['^.*Billy.*$'], 'direct_message,direct_mention', function(bot, message) {
+       bot.reply(message,'Hey watch yourself chief, you are referring to the parmus maximus!');
+    });
+
+	controller.hears(['^where are you?'], 'direct_message,direct_mention', function(bot, message) {
+       bot.reply(message,'Riding the tidal wave on a rusty ship of fools!');
+    });
+	
+	controller.hears(['^what is my pto?'], 'direct_message,direct_mention', function(bot, message) {
+       bot.reply(message,'How the heck am I suppossed to know?? I am not even connected to the TeamHealth network!');
+    });
+	
+    controller.hears(['^say (.*)','^say'], 'direct_message,direct_mention', function(bot, message) {
+        if (message.match[1]) {
+
+            if (!wordfilter.blacklisted(message.match[1])) {
+                bot.reply(message, message.match[1]);
+            } else {
+                bot.reply(message, '_sigh_');
+            }
+        } else {
+            bot.reply(message, 'I will repeat whatever you say.')
+        }
+    });
+
+
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* Utility function to format uptime */
+    function formatUptime(uptime) {
+        var unit = 'second';
+        if (uptime > 60) {
+            uptime = uptime / 60;
+            unit = 'minute';
+        }
+        if (uptime > 60) {
+            uptime = uptime / 60;
+            unit = 'hour';
+        }
+        if (uptime != 1) {
+            unit = unit + 's';
+        }
+
+        uptime = parseInt(uptime) + ' ' + unit;
+        return uptime;
+    }
+
+};
